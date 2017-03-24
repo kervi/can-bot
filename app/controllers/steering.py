5a208b841084b424be025b8479616329ceffda70
Tim Wentzlau <tim.wentzlau@auxilior.com>
2017-03-24 18:10:02 +0000

    added motor handling

--- app/controllers/steering.py
+++ app/controllers/steering.py
@@ -1,14 +1,16 @@
 """ Sample controller """
-from kervi.controller import Controller, UINumberControllerInput, UISwitchButtonControllerInput
+from kervi.controller import Controller, UINumberControllerInput, UIButtonControllerInput
 from kervi.hal import GPIO
+from kervi.steering import MotorSteering
 from kervi_devices.motors.adafruit_i2c_motor_hat import AdafruitMotorHAT
+
 class SteeringController(Controller):
     def __init__(self):
         Controller.__init__(self, "controller.steering", "Manual steering")
         self.type = "motor"
 
         #define an input and link it to the dashboard panel
-        self.all_off_button = UISwitchButtonControllerInput("controller.steering.allOff", "All off", self)
+        self.all_off_button = UIButtonControllerInput("controller.steering.allOff", "All off", self)
         self.all_off_button.link_to_dashboard("app", "steering")
 
         self.speed_input = UINumberControllerInput("controller.steering.speed", "Speed", self)
@@ -23,19 +25,15 @@ class SteeringController(Controller):
         self.direction_input.value = 0
         self.direction_input.link_to_dashboard("app", "steering")
 
-        #define GPIO
-        GPIO.define_as_pwm(12, 50)
+        self.motor_board = AdafruitMotorHAT()
+        self.steering = MotorSteering(self.motor_board.dc_motors[1],self.motor_board.dc_motors[2])
 
     def input_changed(self, changed_input):
-        if changed_input == self.light_button:
-            if changed_input.value:
-                GPIO.pwm_start(12)
-            else:
-                GPIO.pwm_stop(12)
+        if changed_input == self.all_off_button:
+            self.motor_board.dc_motors.stop_all()
 
-        if changed_input.input_id == "lightctrl.level":
-            #change the duty_cycle on the pwm pin
-            GPIO.pwm_start(12, duty_cycle=changed_input.value)
+        if changed_input.input_id == self.speed_input or changed_input.input_id == self.direction_input:
+            self.steering.run(self.speed_input.value, self.direction_input.value)
 
-LightController()
+SteeringController()
 
