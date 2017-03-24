ee5f810a6ac5c44116890c628de1cea34cef7819
Tim Wentzlau <tim.wentzlau@auxilior.com>
2017-03-24 16:54:47 +0000

    update steering

---
+++ app/controllers/steering.py
@@ -0,0 +1,41 @@
+""" Sample controller """
+from kervi.controller import Controller, UINumberControllerInput, UISwitchButtonControllerInput
+from kervi.hal import GPIO
+from kervi_devices.motors.adafruit_i2c_motor_hat import AdafruitMotorHat
+class SteeringController(Controller):
+    def __init__(self):
+        Controller.__init__(self, "controller.steering", "Manual steering")
+        self.type = "motor"
+
+        #define an input and link it to the dashboard panel
+        self.all_off_button = UISwitchButtonControllerInput("controller.steering.allOff", "All off", self)
+        self.all_off_button.link_to_dashboard("app", "steering")
+
+        self.speed_input = UINumberControllerInput("controller.steering.speed", "Speed", self)
+        self.speed_input.min = -100
+        self.speed_input.max = 100
+        self.speed_input.value = 0
+        self.speed_input.link_to_dashboard("app", "steering")
+
+        self.direction_input = UINumberControllerInput("controller.steering.direction", "Direction", self)
+        self.direction_input.min = -100
+        self.direction_input.max = 100
+        self.direction_input.value = 0
+        self.direction_input.link_to_dashboard("app", "steering")
+
+        #define GPIO
+        GPIO.define_as_pwm(12, 50)
+
+    def input_changed(self, changed_input):
+        if changed_input == self.light_button:
+            if changed_input.value:
+                GPIO.pwm_start(12)
+            else:
+                GPIO.pwm_stop(12)
+
+        if changed_input.input_id == "lightctrl.level":
+            #change the duty_cycle on the pwm pin
+            GPIO.pwm_start(12, duty_cycle=changed_input.value)
+
+LightController()
+
--- app/dashboards/__init__.py
+++ app/dashboards/__init__.py
@@ -1,11 +1,11 @@
 """ bootstrap your kervi dashboards here """
-from kervi.dashboard import Dashboard, Camboard, DashboardPanel
+from kervi.dashboard import Dashboard, DashboardPanel
 
 #Create the dashboards for your Kervi application here.
 #Standard dashboard with several panels where sensors are placed.
 #Each sensor create links to one or more dashboard panels 
-APP_DASHBOARD = Dashboard("app", "My dashboard", is_default=True)
-APP_DASHBOARD.add_panel(DashboardPanel("light", columns=2, rows=1, title="Light"))
+APP_DASHBOARD = Dashboard("app", "Main dahsboard", is_default=True)
+APP_DASHBOARD.add_panel(DashboardPanel("steering", columns=2, rows=1, title="Light"))
 APP_DASHBOARD.add_panel(DashboardPanel("sensors", columns=2, rows=1, title="Sensors"))
 
 SYSTEM_DASHBOARD = Dashboard("system", "System")
