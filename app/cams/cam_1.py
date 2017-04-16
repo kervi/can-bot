f8ab357ae248b67c9617562cfdfc69ae9c0814f2
Tim Wentzlau <tim@elektron.fo>
2017-04-16 17:39:08 +0100

    change to PCA9685 servo controller

--- app/cams/cam_1.py
+++ app/cams/cam_1.py
@@ -1,9 +1,9 @@
 import datetime
 from kervi.camera import CameraStreamer
-from kervi_devices.motors.adafruit_i2c_motor_hat import AdafruitMotorHAT
+from kervi_devices.motors.PCA9685_i2c_servo import PCA9685ServoBoard
 
 CAM1 = CameraStreamer("cam1", "camera 1")
 CAM1.link_to_dashboard("app")
-motor_board = AdafruitMotorHAT()
+motor_board = PCA9685ServoBoard()
 motor_board.servo_motors[0].position.link_to(CAM1.pan)
 motor_board.servo_motors[1].position.link_to(CAM1.tilt)
