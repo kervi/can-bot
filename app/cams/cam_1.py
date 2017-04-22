c31810c94bc98e3d92ccb99f6b4daff1d68b2910
Tim Wentzlau <tim@elektron.fo>
2017-04-22 18:10:25 +0100

    Update cam settings

--- app/cams/cam_1.py
+++ app/cams/cam_1.py
@@ -3,6 +3,7 @@ from kervi.camera import CameraStreamer
 from kervi_devices.motors.PCA9685_i2c_servo import PCA9685ServoBoard
 
 CAM1 = CameraStreamer("cam1", "camera 1")
+CAM1.flip_vertical = True
 CAM1.link_to_dashboard("app")
 motor_board = PCA9685ServoBoard()
 motor_board.servo_motors[0].position.link_to(CAM1.pan)
